c2f016bd4f9ebe05e55d24de4723f5b58b27d05c
Jan Tusil <jenda.tusil@gmail.com>
2023-09-23 23:59:56 +0200

    initialize the project

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Minuska - an experimental semantic framework";
+
+  inputs = {
+    nixpkgs.url = "github:NixOs/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+   };
+
+  outputs = { self, nixpkgs, flake-utils }: (
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        coqPackages = pkgs.coqPackages;
+
+      in {
+
+        # The 'matching logic in Coq' library
+        packages.minuska
+        = coqPackages.callPackage 
+        ( { coq, stdenv }:
+        stdenv.mkDerivation {
+          name = "minuska";
+          src = ./minuska;
+
+          propagatedBuildInputs = [
+            coq
+            coqPackages.equations
+            coqPackages.stdpp
+          ];
+          enableParallelBuilding = true;
+          installFlags = [ "COQLIB=$(out)/lib/coq/${coq.coq-version}/" ];
+
+          passthru = { inherit coqPackages; };
+        } ) { } ;
+
+
+        packages.default = self.outputs.packages.${system}.minuska;
+        
+        devShells = {
+          minuska =
+            let
+              minuska = self.outputs.packages.${system}.minuska;
+            in
+              pkgs.mkShell {
+                inputsFrom = [minuska];
+                packages = [minuska.coqPackages.coq-lsp];
+              };
+      }
+    )
+  );
+}
