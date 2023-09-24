23f42e48a80cfbedb23ec6cbd02a7eb42cf4f6b2
Jan Tusil <jenda.tusil@gmail.com>
2023-09-24 16:33:43 +0200

    empty exampels

---
+++ minuska/theories/examples.v
@@ -0,0 +1,18 @@
+From stdpp Require Import base countable decidable list list_numbers gmap.
+(* This is unset by stdpp. We need to set it again.*)
+Set Transparent Obligations.
+
+From Equations Require Import Equations.
+Set Equations Transparent.
+
+Require Import Wellfounded.
+From Ltac2 Require Import Ltac2.
+
+From Minuska Require Import minuska string_variables empty_builtin.
+
+
+Module example_1.
+
+    
+
+End example_1.
---
+++ minuska/theories/string_variables.v
@@ -0,0 +1,9 @@
+From stdpp Require Import base countable decidable list list_numbers gmap strings.
+
+From Minuska Require Import minuska.
+
+From Ltac2 Require Import Ltac2.
+
+Definition StringVariables : Variables := {|
+    variable := string ;
+|}.
