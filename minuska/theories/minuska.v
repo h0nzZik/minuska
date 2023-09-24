7cd70fcbaf0a8a6c0d939829d9e873a4c7d6ea65
Jan Tusil <jenda.tusil@gmail.com>
2023-09-24 02:48:50 +0200

    element_satisfies_pattern_in_valuation

--- minuska/theories/minuska.v
+++ minuska/theories/minuska.v
@@ -1,5 +1,7 @@
 From stdpp Require Import base countable decidable list list_numbers gmap.
 From Equations Require Import Equations.
+Set Equations Transparent.
+Require Import Wellfounded.
 From Ltac2 Require Import Ltac2.
 
 Class Builtin := {
@@ -44,6 +46,8 @@ Inductive Element {Σ : Signature} :=
 | el_builtin (b : builtin_value)
 | el_app (s : symbol) (args : list Element)
 .
+
+(*Derive NoConfusion Subterm EqDec for Element.*)
 
 Fixpoint element_size {Σ : Signature} (e : Element) :=
 match e with
@@ -117,6 +121,16 @@ Inductive Pattern {Σ : Signature} :=
 | pat_requires (p : Pattern) (c : Constraint)
 .
 
+(*Derive NoConfusion Subterm EqDec for Pattern.*)
+
+Fixpoint pattern_size {Σ : Signature} (φ : Pattern) :=
+match φ with
+| pat_builtin _ => 1
+| pat_app s args => 1 + sum_list_with pattern_size args
+| pat_var _ => 1
+| pat_requires p' _ => 1 + pattern_size p'
+end.
+
 Equations? pattern_eqdec' {Σ : Signature} (p1 p2 : Pattern)
     : {p1 = p2} + {p1 <> p2}
     by struct p1
@@ -179,4 +193,45 @@ match c with
 | c_not c' => ~ val_satisfies_c ρ c'
 end.
 
+Section with_signature.
+    Context
+        {Σ : Signature}
+        (ρ : Valuation)
+    .
+
+    Equations element_satisfies_pattern'
+         (φ : Pattern) (e : Element) : Prop
+        by (*wf (@Pattern_subterm Σ)*) struct φ (*wf (pattern_size φ)*) :=
+    element_satisfies_pattern' (pat_builtin b2) (el_builtin b1)
+        := b1 = b2 ;
+    element_satisfies_pattern' (pat_var x) (el_builtin b)
+        := ρ !! x = Some b ;
+    element_satisfies_pattern' (pat_app s2 φs) (el_app s1 es)
+        := s1 = s2
+        /\ length es = length φs
+        /\ elements_satisfies_patterns' φs es ;
+    element_satisfies_pattern' (pat_requires φ' c) e 
+        := element_satisfies_pattern' φ' e 
+        /\ val_satisfies_c ρ c ;
+    element_satisfies_pattern' _ _ := False ;
+    where
+    elements_satisfies_patterns'
+        (φs : list Pattern) (es : list Element) : Prop
+        by struct φs (*wf (sum_list_with pattern_size φs)*) :=
+    elements_satisfies_patterns' [] []
+        := True ;
+    elements_satisfies_patterns' (φ::φs') (e::es')
+        := element_satisfies_pattern' φ e
+        /\ elements_satisfies_patterns' φs' es' ;
+    elements_satisfies_patterns' (e'::es') [] := False ;
+    elements_satisfies_patterns' [] (e'::es') := False
+    .
+
+End with_signature.
+
+Definition element_satisfies_pattern_in_valuation
+    {Σ : Signature} (e : Element) (φ : Pattern) (ρ : Valuation)
+    : Prop :=
+    element_satisfies_pattern' ρ φ e
+.
 
