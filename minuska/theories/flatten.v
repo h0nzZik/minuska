375b69cfbc1183d6317ddb14bd04b5f9534707e8
Jan Tusil <jenda.tusil@gmail.com>
2023-10-10 20:27:37 +0200

    progress!

--- minuska/theories/flatten.v
+++ minuska/theories/flatten.v
@@ -58,6 +58,96 @@ Print AppliedOperator'.
 Print AppliedOperatorOr'.
 Print OpenTerm.
 
+Fixpoint AppliedOperator'_size
+    {Operator Operand : Set}
+    (x : AppliedOperator' Operator Operand)
+    : nat :=
+match x with
+| ao_operator _ => 1
+| ao_app_operand x' _ => 1 + AppliedOperator'_size x'
+| ao_app_ao x1 x2 => 1 + AppliedOperator'_size x1 + AppliedOperator'_size x2
+end.
+
+Definition AppliedOperatorOr'_deep_size
+    {Operator Operand : Set}
+    (x : AppliedOperatorOr' Operator Operand)
+    : nat :=
+match x with
+| aoo_operand _ _ o => 1
+| aoo_app _ _ x' => 1 + AppliedOperator'_size x'
+end.
+
+(*
+Equations AppliedOperatorOr'_symbol_A_to_pair_OpenTerm_SC
+    {Σ : Signature}
+    {A : Set}
+    (A_to_OpenTerm_SC : A ->
+        ((AppliedOperatorOr' symbol BuiltinOrVar) * (list SideCondition))
+    )
+    (x : AppliedOperatorOr' symbol A)
+    : ((AppliedOperatorOr' symbol BuiltinOrVar) * (list SideCondition))
+    by wf (AppliedOperatorOr'_deep_size x)
+:=
+AppliedOperatorOr'_symbol_A_to_pair_OpenTerm_SC f 
+    (aoo_operand _ _ o) := aoo_operand _ _ o ;
+
+AppliedOperatorOr'_symbol_A_to_pair_OpenTerm_SC f
+    (aoo_app _ _ (ao_operator a)) := (aoo_app _ _ (ao_operator a), []) ;
+
+AppliedOperatorOr'_symbol_A_to_pair_OpenTerm_SC f
+    (aoo_app A B (ao_app_operand x2 o))
+    with pair
+        (AppliedOperatorOr'_symbol_A_to_pair_OpenTerm_SC f (aoo_app A B x2))
+        (f o) => {
+            | _,_ := 3
+         } ;
+.
+*)
+(*
+Fixpoint AppliedOperatorOr'_symbol_A_to_pair_OpenTerm_SC
+    {Σ : Signature}
+    {A : Set}
+    (A_to_OpenTerm_SC : A ->
+        ((AppliedOperatorOr' symbol BuiltinOrVar) * (list SideCondition))
+    )
+    (x : AppliedOperatorOr' symbol A)
+    : ((AppliedOperatorOr' symbol BuiltinOrVar) * (list SideCondition))
+:=
+match x with
+| aoo_operand _ _ o => aoo_operand _ _ o
+| aoo_app _ _ (ao_operator a) => (aoo_app _ _ (ao_operator a), [])
+| aoo_app _ _ (ao_app_operand x' o) =>
+    match AppliedOperatorOr'_symbol_A_to_pair_OpenTerm_SC A_to_OpenTerm_SC (aoo_app _ _ x') with
+    | (t1, scs1) =>
+        match A_to_OpenTerm_SC o with
+        | (aoo_app _ _ t2, scs2) =>
+            ((ao_app_operand t1 t2), scs1 ++ scs2)
+        | (aoo_operand _ _ t2, scs2) =>
+            ((ao_app_operand t1 t2), scs1 ++ scs2)
+        end
+    end
+(*
+| ao_app_operand x' o =>
+    match AppliedOperator'_symbol_A_to_pair_OpenTerm_SC A_to_OpenTerm_SC x' with
+    | (t1, scs1) =>
+        match A_to_OpenTerm_SC o with
+        | (aoo_app _ _ t2, scs2) =>
+            ((ao_app_operand t1 t2), scs1 ++ scs2)
+        | (aoo_operand _ _ t2, scs2) =>
+            ((ao_app_operand t1 t2), scs1 ++ scs2)
+        end
+    end
+| ao_app_ao x1 x2 =>
+    match AppliedOperator'_symbol_A_to_pair_OpenTerm_SC A_to_OpenTerm_SC x1 with
+    | (t1, scs1) =>
+        match AppliedOperator'_symbol_A_to_pair_OpenTerm_SC A_to_OpenTerm_SC x2 with
+        | (t2, scs2) => (ao_app_ao t1 t2, scs1 ++ scs2)
+        end
+    end
+*)
+end.
+*)
+
 Fixpoint AppliedOperator'_symbol_A_to_pair_OpenTerm_SC
     {Σ : Signature}
     {A : Set}
@@ -88,6 +178,13 @@ match x with
     end
 end.
 
+(*
+Lemma helper
+    {Σ : Signature}
+    x:
+    match AppliedOperator'_symbol_A_to_pair_OpenTerm_SC A_to_OpenTerm_SC x with
+        | (y, scs) =>
+*)
 Lemma correct_AppliedOperator'_symbol_A_to_pair_OpenTerm_SC
     {Σ : Signature}
     {A : Set}
@@ -120,6 +217,14 @@ Lemma correct_AppliedOperator'_symbol_A_to_pair_OpenTerm_SC
                 AppliedOperator'_symbol_builtin_satisfies_A ρ γ a
             )
     )
+    (correct2_A_to_OpenTerm_SC :
+        ∀ (a : A) (b : builtin_value) (ρ : Valuation),
+        builtin_satisfies_A ρ b a ->
+        ∃ (bov : BuiltinOrVar) rest,
+            (A_to_OpenTerm_SC a) = (aoo_operand _ _ bov, rest)
+            /\ builtin_satisfies_BuiltinOrVar ρ b bov
+            /\ valuation_satisfies_scs ρ rest
+    )
     (x : AppliedOperator' symbol A)
     (g : AppliedOperator' symbol builtin_value)
     :
@@ -148,6 +253,55 @@ Lemma correct_AppliedOperator'_symbol_A_to_pair_OpenTerm_SC
                 x
 .
 Proof.
+    split.
+    { admit. }
+    {
+        intros H.
+        induction H; cbn.
+        {
+            split.
+            { constructor. }
+            { apply Forall_nil. }
+        }
+        {
+            remember (AppliedOperator'_symbol_A_to_pair_OpenTerm_SC A_to_OpenTerm_SC aoxz) as rec.
+            destruct rec as [y0 scs].
+            remember (A_to_OpenTerm_SC z) as occall.
+            destruct occall as [a scs2].
+            apply correct2_A_to_OpenTerm_SC in H0.
+            destruct H0 as [bov [rest [H01 [H02 H03]]]]; cbn in *.
+            rewrite H01 in Heqoccall.
+            destruct a.
+            {
+                ltac1:(exfalso).
+                inversion Heqoccall; subst; clear Heqoccall.
+            }
+            {
+                split.
+                {
+                    constructor.
+                    { apply IHaoxy_satisfies_aoxz. }
+                    {
+                        inversion Heqoccall; subst; clear Heqoccall.
+                        exact H02.
+                    }
+                }
+                {
+                    inversion Heqoccall; subst; clear Heqoccall.
+                    unfold valuation_satisfies_scs.
+                    destruct IHaoxy_satisfies_aoxz as [IH1 IH2]; cbn in *.
+                    rewrite Forall_app.
+                    split.
+                    { exact IH2. }
+                    { exact H03. }
+                }
+            }
+        }
+        {
+            
+        }
+    }
+(*
     revert g.
     induction x; intros g; cbn.
     {
@@ -215,6 +369,7 @@ Proof.
             }
         }
     }
+*)
 Qed.
 
 Print LhsPattern.
