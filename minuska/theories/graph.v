9fb37e64765ecde11f03295c2d39ded5361cf2a9
Traian Florin Serbanuta <traian.serbanuta@runtimeverification.com>
2023-11-24 16:42:36 +0200

    Progress

--- minuska/theories/graph.v
+++ minuska/theories/graph.v
@@ -1,6 +1,6 @@
-From Minuska Require Import
-    prelude
-.
+From stdpp Require Import prelude.
+From Equations Require Import Equations.
+Import Equations.Prop.Logic.
 
 Section sec_graph.
 
@@ -34,19 +34,22 @@ Context
 Fixpoint take_until (l : list T) : list T :=
   match l with
   | [] => []
-  | x :: t =>x ::
+  | x :: t => x ::
     if (decide (P x)) then [] else take_until t
   end. 
+
+Lemma take_until_cons :
+  forall (l : list T) (x : T), ~ P x ->
+  take_until (x :: l) = x :: take_until l.
+Proof. by intros; cbn; case_decide. Qed.
 
 Lemma take_until_is_take (l : list T) :
   exists n, take_until l = take n l.
 Proof.
-  induction l.
-  - exists 0; reflexivity.
-  - cbn; ltac1: (case_decide).
-    + exists 1; reflexivity.
-    + destruct IHl as [n ->].
-      exists (S n); reflexivity.
+  induction l; [by exists 0 |].
+  cbn; case_decide; [by exists 1 |].
+  destruct IHl as [n ->].
+  by exists (S n).
 Qed.
 
 Lemma take_until_subseteq (l : list T) :
@@ -85,15 +88,13 @@ Proof.
 intros.
 remember (size _) as size1.
 remember (size ((_ ∪ (_ ∪ _)) ∖ _)) as size2.
-destruct (decide (size1 = size2)) as [-> |].
-  + ltac1 : (apply right_lex; lia).
-  + apply left_lex.
-    ltac1: (cut (size1 <= size2); [lia |]).
-    subst; apply subseteq_size; intro y.
-    ltac1: (rewrite !elem_of_difference).
-    intros []; split.
-    * ltac1: (set_solver).
-    * assumption.
+destruct (decide (size1 = size2)) as [-> |];
+  [by apply right_lex; lia |].
+  apply left_lex.
+  cut (size1 <= size2); [lia |].
+  subst; apply subseteq_size; intro y.
+  rewrite !elem_of_difference.
+  by intros []; split; [set_solver |].
 Qed.
 
 Equations? find_cycle (explore path : list T) (explored : TSet) : find_cycle_result
@@ -109,7 +110,7 @@ with (decide (x ∉ nodes)) => {
     with inspect (one_step_reachable x) => {
     | exist _ x_next _
       with inspect (filter (fun x => x ∈ path) x_next) => {
-      | exist _ (y :: _) _ => has_cycle (rev (x :: take_until (fun z => z = y) path));
+      | exist _ (y :: _) _ => has_cycle (reverse (x :: take_until (fun z => z = y) path));
       | exist _ [] _
         with find_cycle x_next (x :: path) explored => {
         | has_cycle c => has_cycle c;
@@ -123,25 +124,19 @@ Proof.
 - apply find_cycle_obligation_helper.
 - apply find_cycle_obligation_helper.
 - apply left_lex.
-  assert (Hrew :
-    list_to_set (C := TSet) nodes ∪ list_to_set (one_step_reachable x)
-      ≡ list_to_set nodes).
+  assert (list_to_set (C := TSet) nodes ∪ list_to_set (one_step_reachable x)
+      ≡ list_to_set nodes) as ->.
   {
-    unfold one_step_reachable; intro y; rewrite elem_of_union.
-    ltac1: (rewrite !elem_of_list_to_set; rewrite elem_of_list_filter).
-    ltac1: (set_solver).
+    unfold one_step_reachable; intro y.
+    rewrite elem_of_union, !elem_of_list_to_set, elem_of_list_filter.
+    by set_solver.
   }
-  rewrite Hrew.
   apply subset_size.
-  split.
-  + intro y.
-    ltac1: (rewrite !elem_of_difference; set_solver).
+  split; [by intro; rewrite !elem_of_difference; set_solver |].
   + intros Hsub.
     specialize (Hsub x).
-    ltac1: (rewrite !elem_of_difference in Hsub).
-    ltac1: (rewrite !elem_of_union in Hsub).
-    ltac1: (rewrite !elem_of_list_to_set in Hsub).
-    ltac1: (set_solver).
+    rewrite !elem_of_difference, !elem_of_union, !elem_of_list_to_set in Hsub.
+    by set_solver.
 - apply find_cycle_obligation_helper.
 Qed.
 
@@ -160,16 +155,13 @@ Lemma graph_walk_from_to_last (x y : T) (w : list T) :
   graph_walk_from_to x y w -> w <> [] -> list.last w = Some y.
 Proof.
   intros Hwalk Hnil.
-  induction Hwalk.
-  - ltac1:(by contradict Hnil).
-  - inversion Hwalk; subst.
-    + ltac1:(done).
-    + ltac1: (by apply IHHwalk).
+  induction Hwalk; [by contradict Hnil |].
+  by inversion Hwalk; subst; [| apply IHHwalk].
 Qed.
 
 Inductive graph_walk : list T -> Prop :=
-| gw : forall (x : T) (w : list T),
-    graph_walk_from_to x (last w x) w -> graph_walk (x :: w).
+| gw : forall (x y : T) (w : list T),
+    graph_walk_from_to x y w -> graph_walk (x :: w).
 
 Record graph_path_from_to (x y : T) (w : list T) : Prop :=
 {
@@ -178,8 +170,27 @@ Record graph_path_from_to (x y : T) (w : list T) : Prop :=
 }.
 
 Inductive graph_path : list T -> Prop :=
-| gp : forall (x : T) (w : list T),
-    graph_path_from_to x (last w x) w -> graph_path (x :: w).
+| gp : forall (x y : T) (w : list T),
+    graph_path_from_to x y w -> graph_path (x :: w).
+
+Lemma graph_path_suffix :
+  forall (l suf : list T), suf `suffix_of` l -> suf <> [] ->
+  graph_path l -> graph_path suf.
+Proof.
+  intros l suf [pre ->] Hsuf.
+  inversion 1 as [h lst t Hpath Heq].
+  destruct suf as [| hsuf]; [done |].
+  constructor 1 with lst.
+  destruct pre; simplify_list_eq; [done |].
+  destruct Hpath as [Hwalk Hnodup].
+  change (t0 :: pre ++ hsuf :: suf) with ((t0 :: pre) ++ (hsuf :: suf)) in Hnodup.
+  apply NoDup_app in Hnodup as (_ & _ & Hnodup).
+  split; [| done].
+    Search (List.last (_::_)).
+  clear -Hwalk; revert t0 Hwalk; induction pre;
+    [by inversion 1; subst |].
+  by inversion 1; subst; eapply IHpre.
+Qed.
 
 Record graph_cycle_from_to (x y : T) (w : list T) : Prop :=
 {
@@ -189,7 +200,7 @@ Record graph_cycle_from_to (x y : T) (w : list T) : Prop :=
 
 Inductive graph_cycle : list T -> Prop :=
 | gc : forall (x : T) (w : list T),
-    graph_cycle_from_to x (last w x) w ->
+    graph_cycle_from_to x (List.last w x) w ->
     graph_cycle (x :: w).
 
 Record graph_contains_cycle_witness (nodes c : list T) : Prop :=
@@ -210,31 +221,58 @@ Proof.
   clear Heqexplore.
   remember ∅ as explored; clear Heqexplored.
   remember [] as path.
-  assert (Hpath : Forall (fun x => graph_path (rev (x :: path))) explore).
+  assert (Hpath : Forall (fun x => graph_path (reverse (x :: path))) explore).
   {
     apply list.Forall_forall; intros x Hx; subst; cbn.
-    repeat constructor.
+    constructor 1 with x; repeat constructor.
     apply not_elem_of_nil.
   }
-  assert (Hpath_sub : path ⊆ nodes) by ltac1: (subst; set_solver).
+  assert (Hpath_sub : path ⊆ nodes) by (subst; set_solver).
   clear Heqpath.
-  ltac1: (funelim (find_cycle nodes explore path explored)).
-  - ltac1: (congruence).
+  funelim (find_cycle nodes explore path explored).
+  - done.
   - apply H8.
-    + rewrite Heqcall; assumption.
-    + ltac1: (set_solver).
-    + inversion Hpath; assumption.
-    + assumption.
+    + by rewrite Heqcall.
+    + by set_solver.
+    + by inversion Hpath.
+    + done.
   - apply H8.
-    + rewrite Heqcall; assumption.
-    + ltac1: (set_solver).
-    + inversion Hpath; assumption.
-    + assumption.
+    + by rewrite Heqcall.
+    + by set_solver.
+    + by inversion Hpath.
+    + done.
   - rewrite <- Heqcall in Heq1.
     inversion Heq1; subst.
     inversion Hpath; subst.
+    (*
+    inversion H11 as [hp tp [Hw Hnodup] Heqp].
+    rewrite Heqp, reverse_Permutation in Hnodup.
+    apply NoDup_cons in Hnodup as [Hx Hnodup].
+    *)
+    assert (y ∈ path /\ R x y) as [Hy Hxy].
+    {
+      inversion H8 as [Hfilter].
+      assert (Helem : y ∈ y :: l) by left.
+      rewrite <- Hfilter in Helem.
+      apply elem_of_list_filter in Helem as [Helem Hy].
+      split; [done |].
+      by apply elem_of_list_filter in Hy as [].
+    }
+    assert (x <> y).
+    {
+      intros ->; apply n.
+      rewrite elem_of_union, elem_of_list_to_set.
+      by right.
+    }
     split.
-    + 
+    + rewrite reverse_Permutation.
+      intro v.
+      rewrite elem_of_cons; intros [-> |];
+        [by apply Hsub; left |].
+      by eapply Hpath_sub, take_until_subseteq.
+    + rewrite <- take_until_cons by done.
+      assert (exists path', take_until (λ z : T, z = y) (x :: path))
+      
 Admitted.
 
 Definition is_reachable_from
