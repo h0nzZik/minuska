2056689d77b45421e1fc69b22d31eeefb90bd561
Jan Tusil <jenda.tusil@gmail.com>
2024-01-10 16:27:53 +0100

    integrate 'rec_normalize' into 'rec.sh'

--- rec.sh
+++ rec.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-INPUT_FILE="$1"
+DIR=`dirname "$0"`
+
+INPUT_FILE=$(realpath "$1")
+
+# TODO handle this in a better way (mktemp?)
+TMP_FILE="$INPUT_FILE.tmp"
+
+"$DIR/rec-2015-convecs/com/rec_normalize" -nothing "$INPUT_FILE" > "$TMP_FILE"
+
+#cat "$TMP_FILE"
 
 inputWithoutComments() {
-    cat "$INPUT_FILE" \
+    cat "$TMP_FILE" \
         | cut -f1 -d"%"
 }
 
@@ -33,9 +42,15 @@ vars() {
 
 rules() {
     inputWithoutComments \
-        | sed -e '1,/RULES/d'
+        | sed -e '1,/RULES/d' \
+        | sed -n '/EVAL/q;p'
 }
 
+evalSection() {
+    inputWithoutComments \
+        | sed -e '1,/EVAL/d' \
+        | sed -n '/END-SPEC/q;p'
+}
 
 cons \
     | cut --delimiter=':' --fields='1' \
@@ -54,9 +69,8 @@ vars \
     | sed -e 's/ /\n/g' \
     | sed -e 's/\(.*\)/Definition \1 := \"\1\"\./'
 
-echo 'Definition rules := ['
-
-rules \
+processSymbolicTerm() {
+    cat - \
     | sed 's/^[ \t]*//;s/[ \t]*$//'  \
     | sed -e 's/\([a-z]\([[:alnum:]]\)*\),/\1(),/g' \
     | sed -e 's/\([a-z]\([[:alnum:]]\)*\))/\1())/g' \
@@ -64,8 +78,22 @@ rules \
     | sed -e 's/\([A-Z]\)/$\1/g' \
     | sed -e 's/(/[</g' \
     | sed -e 's/)/>]/g' \
+    
+}
+
+processRule() {
+    cat - \
     | sed -e 's/\(.*\)->\(.*\)/rule "top"[<\1>] => "top"[<\2>] requires []/g' \
     | sed -e 's/\(.*\)/\1;/' \
     | head --bytes=-2
+}
+
+echo 'Definition rules := ['
+rules | processSymbolicTerm | processRule
 
 echo '].'
+
+echo 'Definition term_to_reduce :='
+evalSection | processSymbolicTerm
+echo '.'
+
