a9cf3ba92f394e86b12365b2ee0ec53c42c0dd6d
Jan Tusil <jenda.tusil@gmail.com>
2024-01-14 13:37:23 +0100

    Revert "weird commit to be reverted"
    This reverts commit cfb386e4191e1f5fd66bb7092f6c422d956628ce.

--- minuska/theories/notations.v
+++ minuska/theories/notations.v
@@ -8,147 +8,58 @@ From Minuska Require Import
 .
 
 
-Declare Scope ExprScope.
 Declare Scope RuleLhsScope.
-(*Declare Scope RuleRhsScope.*)
+Declare Scope RuleRhsScope.
 Declare Scope RuleScsScope.
 Declare Scope ConcreteScope.
 
-Delimit Scope ExprScope with expr.
 Delimit Scope RuleLhsScope with rule_lhs.
-(*Delimit Scope RuleRhsScope with rule_rhs.*)
+Delimit Scope RuleRhsScope with rule_rhs.
 Delimit Scope RuleScsScope with rule_scs.
 Delimit Scope ConcreteScope with concrete.
 
-(*
-Section wm.
-    Context
-        {Σ : StaticModel}
-    .
-
-    Check AppliedOperator' symbol (Type -> Type).
-
-End wm.
-
-*)
-
-Class MyContext (Operand : Type) := {
-    mc_result : Type;
-    mc_result_from_operand : Operand -> mc_result ;
+Structure MyApplyLhs {Σ : StaticModel} := {
+    mal_T2 : Type ;
+    my_apply_lhs :
+        AppliedOperator' symbol BuiltinOrVar ->
+        mal_T2 ->
+        AppliedOperator' symbol BuiltinOrVar ;
 }.
 
-Definition MCAO {Σ : StaticModel} (Operand : Type) : Type
-:=
-    AppliedOperator'
-        symbol
-        (forall (MC : MyContext Operand), mc_result)
-.
+Arguments my_apply_lhs {_} {m} _ _.
+Arguments my_apply_lhs : simpl never.
 
-Definition mc_operator
-    {Σ : StaticModel}
-    {Operand : Type}
-    (sym : symbol)
-    : MCAO Operand
-:=
-    ao_operator sym
-.
+Definition MyApplyLhs_operand {Σ : StaticModel} : MyApplyLhs := {|
+    my_apply_lhs := fun x y => ao_app_operand x y ;
+|}.
+Canonical MyApplyLhs_operand.
 
-Definition mc_app_operand
-    {Σ : StaticModel}
-    {Operand : Type}
-    (app : MCAO Operand)
-    (operand : Operand)
-    : MCAO Operand
-:=
-    fun MC => ao_app_operand (app MC) operand
-.
-
-Definition mc_app_ao
-    {Σ : StaticModel}
-    {Operand : Type}
-    (app1 app2 : MCAO Operand)
-    : MCAO Operand
-:=
-    fun MC => ao_app_ao (app1 MC) (app2 MC)
-.
-
-Definition MC_resolve
-    {Σ : StaticModel}
-    {Operand : Type}
-    (x : MCAO Operand)
-    (MC : MyContext Operand)
-    : AppliedOperator' symbol mc_result
-:= x MC.
-    match x with
-    |
-    end
-.
+Definition MyApplyLhs_ao {Σ : StaticModel} : MyApplyLhs := {|
+    my_apply_lhs := fun x y => @ao_app_ao symbol BuiltinOrVar x y ;
+|}.
+Canonical MyApplyLhs_ao.
 
 
-Inductive BoV_or_Expr := BE_BoV | BE_Expr .
-
-Structure InterpretBE {Σ : StaticModel} := {
-    ibe_BE : BoV_or_Expr ;
-    ibe_type : Type ;
+Structure MyApplyRhs {Σ : StaticModel} := {
+    mar_T2 : Type ;
+    my_apply_rhs :
+        AppliedOperator' symbol Expression ->
+        mar_T2 ->
+        AppliedOperator' symbol Expression ;
 }.
 
-Definition InterpretBE_BoV {Σ : StaticModel} : InterpretBE := {|
-    ibe_BE := BE_BoV ;
-    ibe_type := BuiltinOrVar ;
+Arguments my_apply_rhs {_} {m} _ _.
+Arguments my_apply_rhs : simpl never.
+
+Definition MyApplyRhs_operand {Σ : StaticModel} : MyApplyRhs := {|
+    my_apply_rhs := fun x y => ao_app_operand x y ;
 |}.
-Canonical InterpretBE_BoV.
+Canonical MyApplyRhs_operand.
 
-Definition InterpretBE_Expr {Σ : StaticModel} : InterpretBE := {|
-    ibe_BE := BE_Expr ;
-    ibe_type := Expression ;
+Definition MyApplyRhs_ao {Σ : StaticModel} : MyApplyRhs := {|
+    my_apply_rhs := fun x y => @ao_app_ao symbol Expression x y ;
 |}.
-Canonical InterpretBE_Expr.
-
-(*
-Inductive Operand_or_AO := OA_Operand | OA_AO.
-
-Structure InterpretOA {Σ : StaticModel} := {
-    ioa_OA : Operand_or_AO ;
-    ioa_type : Type ;
-}.
-*)
-
-Structure MyApply
-    {Σ : StaticModel}
-:= {
-    ma_A : InterpretBE;
-    ma_B : Type ;
-    my_apply :
-        AppliedOperator' symbol (ibe_type ma_A) ->
-        ma_B ->
-        AppliedOperator' symbol (ibe_type ma_A) ;
-}.
-
-Arguments my_apply {_} {m} _ _.
-Arguments my_apply : simpl never.
-
-Definition MyApply_operand
-    {Σ : StaticModel}
-    (A : InterpretBE)
-    : @MyApply Σ
-:= {|
-    ma_A := A ;
-    ma_B := (ibe_type A) ;
-    my_apply := @ao_app_operand symbol (ibe_type A) ;
-|}.
-Canonical MyApply_operand.
-
-
-Definition MyApply_ao
-    {Σ : StaticModel}
-    {A : InterpretBE}
-    : @MyApply Σ
-:= {|
-    ma_A := A ;
-    ma_B := (AppliedOperator' symbol (ibe_type A)) ;
-    my_apply := @ao_app_ao symbol (ibe_type A) ;
-|}.
-Canonical MyApply_ao.
+Canonical MyApplyRhs_ao.
 
 
 Structure MyApplyConcrete {Σ : StaticModel} := {
@@ -177,15 +88,15 @@ Notation "f [<>]" := (ao_operator f)
 .
 
 Notation "f [< y , .. , z >]"
-    := (my_apply .. (my_apply (ao_operator f) y) .. z)
+    := (my_apply_lhs .. (my_apply_lhs (ao_operator f) y) .. z)
     (at level 90)
     : RuleLhsScope
 .
 
 Notation "f [< y , .. , z >]"
-    := (my_apply .. (my_apply (ao_operator f) y) .. z)
+    := (my_apply_rhs .. (my_apply_rhs (ao_operator f) y) .. z)
     (at level 90)
-    : ExprScope
+    : RuleRhsScope
 .
 
 Notation "f [< y , .. , z >]"
@@ -193,22 +104,28 @@ Notation "f [< y , .. , z >]"
     (at level 90)
     : ConcreteScope
 .
-Locate "~".
+
 Notation "'$' x" := (bov_variable x)
-    (at level 10)
+    (at level 200)
     : RuleLhsScope
 .
 
 Notation "'$' x" := (ft_variable x)
-    (at level 10)
-    : ExprScope
+    (at level 200)
+    : RuleRhsScope
+.
+
+
+Notation "'$' x" := (ft_variable x)
+    (at level 200)
+    : RuleScsScope
 .
 
 Notation "'llrule' l => r 'requires' s"
     := (@mkFlattenedRewritingRule
         _
         (l)%rule_lhs
-        (r)%expr
+        (r)%rule_rhs
         (s)%rule_scs
     )
     (at level 200)
@@ -224,16 +141,3 @@ Notation "'rule' l => r 'requires' s"
     )
     (at level 200)
 .
-
-(*
-Notation "'[^' f x '^]'" :=
-    (ft_unary f (x)%expr)
-    : ExprScope
-.
-
-Notation "'[^' f  x  y '^]'" :=
-    (ft_binary f (x)%expr (y)%expr)
-    : ExprScope
-.
-*)
-
