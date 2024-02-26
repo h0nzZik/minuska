562196c7cf4e564e2b8bf81f29c3fe2a75af67ad
Jan Tusil <jenda.tusil@gmail.com>
2024-02-26 14:03:31 +0100

    almost there...

--- minuska/theories/minusl_compile.v
+++ minuska/theories/minusl_compile.v
@@ -6778,10 +6778,45 @@ Proof.
                             }
                         }
                         {
-
+                            ltac1:(exfalso).
                         }
                         {
-
+                            specialize (H t).
+                            ltac1:(ospecialize (H _)).
+                            {
+                                rewrite elem_of_list_lookup.
+                                exists i.
+                                exact Hli.
+                            }
+                            specialize (H t0 γ1 γ2 ψ).
+                            specialize (H ltac:(assumption)).
+                            specialize (H ltac:(assumption)).
+                            simpl in Hnotsub.
+                            destruct (decide (t_term s l = TermOverBuiltin_to_TermOverBoV γ1)) as [His|Hisnot].
+                            {
+                                simpl in Hnotsub. inversion Hnotsub.
+                            }
+                            {
+                                simpl in Hnotsub.
+                                assert (Hnot' := @existsb_nth (TermOver BuiltinOrVar)).
+                                specialize (Hnot' (is_subterm_b (TermOverBuiltin_to_TermOverBoV γ1)) l).
+                                specialize (Hnot' i t).
+                                ltac1:(ospecialize (Hnot' _)).
+                                {
+                                    apply lookup_lt_Some in Hli.
+                                    exact Hli.
+                                }
+                                specialize (Hnot' Hnotsub).
+                                assert (Hli' := Hli).
+                                apply nth_lookup_Some with (d := t) in Hli'.
+                                rewrite Hli' in Hnot'.
+                                specialize (H Hnot').
+                                clear Hli' Hnot'.
+                                specialize (H Hnotincod).
+                                specialize (H HH1).
+                                specialize (H HH4).
+                                exact H.
+                            }
                         }
                         {
                             rewrite not_subterm_subst>[|assumption].
