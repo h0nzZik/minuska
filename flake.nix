e4bbabb81b638767e9905b5e10aa13b4f3347d5c
Jan Tusil <jenda.tusil@gmail.com>
2024-04-05 14:35:09 +0200

    fix again

--- flake.nix
+++ flake.nix
@@ -152,7 +152,7 @@
             in
               pkgs.mkShell {
                 inputsFrom = [minuska];
-                packages = [minuska.coqPackages.coq-lsp minuska.coqPackages.coqide minuska.coqPackages.vscoq-language-server];
+                packages = [minuska.coqPackages.coq-lsp minuska.coqPackages.coqide];
               };
 
 
