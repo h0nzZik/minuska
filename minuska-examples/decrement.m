9a21c8684cc8d255b1c73c40052106cb2d6e3919
Jan Tusil <jenda.tusil@gmail.com>
2024-04-18 13:55:28 +0200

    decrement works again

--- minuska-examples/decrement.m
+++ minuska-examples/decrement.m
@@ -1,24 +1,10 @@
-@frames: [
-   fr(X): u_cfg [ u_state [ u_cseq [ X, REST ], VALUES ] ]
-];
+@frames: [];
 
 @value(X): (bool.false()) ;
 
-@strictness: [
-	if of_arity 3 in [0]
-];
+@strictness: [];
 
-
-@rule/fr [if.true]: if[true[], T, F] => T where bool.true();
-@rule/fr [if.false]: if[false[], T, F] => B where bool.true();
-
-@rule/fr [decrement]:
-	succ[X] => X where bool.true()
-    ;
-
-@rule [decrement]:
-	succ[X] => X where bool.true()
-    ;
+@rule [decrement]: succ[X] => X where bool.true() ;
 
 
 
