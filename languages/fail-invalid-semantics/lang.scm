ab09b739063c2a05c1f94d79675490ec450e53fa
Jan Tusil <jenda.tusil@gmail.com>
2024-06-27 20:27:57 +0200

    forgotten files

---
+++ languages/fail-invalid-semantics/lang.scm
@@ -0,0 +1,7 @@
+(
+ (language fail-invalid-semantics)
+ (semantics fail-invalid-semantics.m)
+ (parser_exe "parser/_build/install/default/bin/parser")
+ (parser_builder "cd parser; dune build @all")
+)
+
