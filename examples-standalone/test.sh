2468786901333003d846a90ceb95ce69e1ef1bb7
Jan Tusil <jenda.tusil@gmail.com>
2024-06-29 14:35:30 +0200

    nicer output of benchmarks

--- examples-standalone/test.sh
+++ examples-standalone/test.sh
@@ -5,7 +5,7 @@ set -e
 # https://stackoverflow.com/a/246128/6209703
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-pushd "$SCRIPT_DIR"
+pushd "$SCRIPT_DIR" > /dev/null
 
 TEMPDIR="$(pwd)/_temp"
 LOGFILEOUT="$TEMPDIR/log-stdout.txt"
@@ -37,23 +37,24 @@ runCase() {
   local expout="$5"
 
   output=$(mktemp)
-  echo "Running test '$name'"
+  #echo "Running test '$name'"
   "$TIME" --output "$output.time" --format "%e" "$interpreter" --depth "$depth" --output-file "$output" "$input" 2>>"$LOGFILEERR" >>"$LOGFILEOUT"
   if [[ -e "$expout" ]]; then
     diff "$output" "$expout"
   fi
-  cat "$output.time"
+  local mytime=$(cat "$output.time")
+  echo "$name,  $mytime"
   rm -f "$output"
 }
 
 doCompile() {
   local lang="$1"
-  echo "Compiling: minuska generate-interpreter $lang" 
+  #echo "Compiling: minuska generate-interpreter $lang" 
   mkdir -p interpreters
-  pushd interpreters
-  minuska generate-interpreter ../languages/$lang/lang.scm 2>>"$LOGFILEERR" >>"$LOGFILEOUT" && echo "Compilation finished."
+  pushd interpreters > /dev/null
+  minuska generate-interpreter ../languages/$lang/lang.scm 2>>"$LOGFILEERR" >>"$LOGFILEOUT" #&& echo "Compilation finished."
   local state=$?
-  popd
+  popd > /dev/null
   return $state
 }
 
@@ -67,26 +68,31 @@ testNative() {
     exit 1
   fi
 
-
+  echo "Compiling..."
   doCompile decrement
-  echo "Decrement tests"
-  runCase "dec into 2" ./interpreters/decrement-interpreter ./languages/decrement/tests/three.dec 2 DONOTTTEST
-  runCase "dec into 1" ./interpreters/decrement-interpreter ./languages/decrement/tests/three.dec 3 DONOTTTEST
-
-
   doCompile decrement-builtin
-  runCase "dec builtin into -1" ./interpreters/decrement-builtin-interpreter ./languages/decrement-builtin/tests/cfg_3.decb 5 ./languages/decrement-builtin/tests/cfg_minus_1
-
   doCompile arith
+  doCompile imp
+  doCompile two-counters
+
+
+  echo "Native (via OCaml extraction) benchmarks"
+  echo "name, execution time"
+
+  #echo "Decrement tests"
+  runCase "dec-into-2" ./interpreters/decrement-interpreter ./languages/decrement/tests/three.dec 2 DONOTTTEST
+  runCase "dec-into-1" ./interpreters/decrement-interpreter ./languages/decrement/tests/three.dec 3 DONOTTTEST
+
+
+  runCase "dec-builtin-into--1" ./interpreters/decrement-builtin-interpreter ./languages/decrement-builtin/tests/cfg_3.decb 5 ./languages/decrement-builtin/tests/cfg_minus_1
+
   runCase "arith-01" ./interpreters/arith-interpreter ./languages/arith/tests/01.arith 20 ./languages/arith/tests/01.result
 
-  doCompile imp
   runCase "imp-01" ./interpreters/imp-interpreter ./languages/imp/tests/01.imp 20 ./languages/imp/tests/01.result
   #runCase "imp-lookup" ./interpreters/imp-interpreter ./languages/imp/tests/00-assign-lookup-trivial.imp 20 ./languages/imp/tests/00-assign-lookup-trivial.result
   runCase "imp-count-10" ./interpreters/imp-interpreter ./languages/imp/tests/03-count-10.imp 1000 ./languages/imp/tests/03-count-10.result
 
 
-  doCompile two-counters
   runCase "two-counters.10"        ./interpreters/two-counters-interpreter ./languages/two-counters/tests/10.tc      50000000 ./languages/two-counters/tests/10.result
   runCase "two-counters.100"       ./interpreters/two-counters-interpreter ./languages/two-counters/tests/100.tc     50000000 ./languages/two-counters/tests/100.result
   runCase "two-counters.1'000"     ./interpreters/two-counters-interpreter ./languages/two-counters/tests/1000.tc    50000000 ./languages/two-counters/tests/1000.result
