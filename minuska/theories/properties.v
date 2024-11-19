94c7ceddfea809539730fbfb5cf8d9538fff1ece
Jan Tusil <jenda.tusil@gmail.com>
2024-11-19 23:47:18 +0100

    length_map

--- minuska/theories/properties.v
+++ minuska/theories/properties.v
@@ -1277,7 +1277,7 @@ Proof.
         ltac1:(simp sat2B).
         simpl.
         split>[reflexivity|].
-        rewrite map_length.
+        rewrite length_map.
         split>[reflexivity|].
         intros i t' φ' HH1 HH2.
         ltac1:(replace (map) with (@fmap list list_fmap) in HH1 by reflexivity).
