d0ef14014a7fc717063e9d0816f5310f9f142fde
Jan Tusil <jenda.tusil@gmail.com>
2024-12-28 19:44:07 +0100

    two_counters

--- languages/two-counters/def.m
+++ languages/two-counters/def.m
@@ -4,7 +4,7 @@
 @context(HOLE): cfg [ HOLE ];
 @strictness: [];
 
-@rule [init]: builtin.init[X] => state[X, [(@builtin-int 0)]] where [] ;
+@rule [init]: builtin.init[] => state[program.ast(), [(@builtin-int 0)]] where [] ;
 
 @rule [step]: state[M, N] => state[z.minus(M, [(@builtin-int 1)]), z.plus(N, M)] where [is_true(z.lt([(@builtin-int 0)], M))] ;
 
---
+++ languages/two-counters/transform.ml
@@ -0,0 +1,21 @@
+open Core
+
+let print_position lexbuf =
+  let pos = lexbuf.Lexing.lex_curr_p in
+  sprintf "%s:%d:%d" pos.pos_fname pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
+
+let parse_with_error lexbuf =
+  try Parser.option_int Lexer.read lexbuf with
+  | Lexer.SyntaxError msg ->
+      raise (Invalid_argument ("Lexing problem at " ^ (print_position lexbuf) ^ ": " ^ msg))
+  | Parser.Error ->
+      raise (Invalid_argument ("Parsing problem at " ^ (print_position lexbuf) ^ "."))
+
+let convert_int (n : int) : Libminuska.Syntax.groundterm =
+  `GTb (`BuiltinInt n)
+      
+let parse (lexbuf : Lexing.lexbuf) : Libminuska.Syntax.groundterm =
+  match parse_with_error lexbuf with
+  | Some value -> convert_int value
+  | None -> raise (Invalid_argument "Empty file?")
+    
