043122c25786b067d80502c412c16231186e1061
Jan Tusil <jenda.tusil@gmail.com>
2025-07-23 12:08:47 +0100

    fix spec_interpreter.v

--- coq-minuska/theories/spec_interpreter.v
+++ coq-minuska/theories/spec_interpreter.v
@@ -9,9 +9,10 @@ Definition not_stuck
     {Label : Set}
     (Γ : list (RewritingRule2 Label))
     (program : ProgramT)
+    (h : hidden_data)
     (e : TermOver builtin_value) : Type
 :=
-    { e' : _ & { nv : NondetValue & rewriting_relation Γ program nv e e' } }
+    { e' : _ & { nv : NondetValue & rewriting_relation Γ program h nv e e' } }
 .
 
 Definition stuck
@@ -19,9 +20,10 @@ Definition stuck
     {Label : Set}
     (Γ : list (RewritingRule2 Label))
     (program : ProgramT)
+    (h : hidden_data)
     (e : TermOver builtin_value) : Type
 :=
-    notT (not_stuck Γ program e)
+    notT (not_stuck Γ program h e)
 .
 
 
@@ -30,7 +32,7 @@ Definition Interpreter
     {Label : Set}
     (Γ : list (RewritingRule2 Label))
     : Type
-    := ProgramT -> NondetValue -> TermOver builtin_value -> option (TermOver builtin_value)
+    := ProgramT -> hidden_data -> NondetValue -> TermOver builtin_value -> option (TermOver builtin_value)
 .
 
 Definition Interpreter_ext
@@ -38,7 +40,7 @@ Definition Interpreter_ext
     {Label : Set}
     (Γ : list (RewritingRule2 Label))
     : Type
-    := ProgramT -> NondetValue -> TermOver builtin_value -> option ((TermOver builtin_value)*nat)
+    := ProgramT -> hidden_data -> NondetValue -> TermOver builtin_value -> option ((TermOver builtin_value)*nat)
 .
 
 
@@ -49,16 +51,16 @@ Definition Interpreter_sound'
     (interpreter : Interpreter Γ)
     : Type
     := ((
-        forall program e1 e2 nv,
-            interpreter program nv e1 = Some e2 ->
-            rewriting_relation Γ program nv e1 e2
+        forall program h e1 e2 nv,
+            interpreter program h nv e1 = Some e2 ->
+            rewriting_relation Γ program h nv e1 e2
     )
     *
-    (forall program e,
-        stuck Γ program e -> forall nv, interpreter program nv e = None)
-    * (forall program e,
-        not_stuck Γ program e ->
-        exists e' (nv : NondetValue), interpreter program nv e = Some e')
+    (forall program h e,
+        stuck Γ program h e -> forall nv, interpreter program h nv e = None)
+    * (forall program h e,
+        not_stuck Γ program h e ->
+        exists e' (nv : NondetValue), interpreter program h nv e = Some e')
     )%type
 .
 
