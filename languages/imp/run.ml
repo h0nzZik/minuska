f72f88a6284e87982391cbf5ca3d4dc131d65887
Jan Tusil <jenda.tusil@gmail.com>
2025-08-15 08:58:06 +0100

    fixes to IMP

--- languages/imp/run.ml
+++ languages/imp/run.ml
@@ -4,18 +4,11 @@ open Libminuskapluginbase
 open Pluginbase
 
 let main () =
-  let pvae = (get_primitive_value_algebra (coqModuleName_of_sexp (Sexp.of_string ("(Std_module klike)")))) in
-  let signature = (pvae.pvae_builtin_interface.bi_signature) in
-  let builtins = (pvae.pvae_builtin_interface.bi_beta) in
-  let pie = (get_trivial_program_info signature builtins) in
-
   Libminuska.Miskeleton.main
-    pvae
-    (get_default_hidden_algebra_with_klike ())
-    pie
-    Imp.Transform.parse
-    Imp.Internal.langDefaults
-    Imp.Internal.lang_Decls
+    Libminuska.Miskeleton.klike_interface
+    Transform.parse
+    Internal.langDefaults
+    Internal.lang_Decls
   
 let _ = main ()
     
