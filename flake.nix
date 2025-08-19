6bc4eddf47ec8af38051ba36268d40c4880b011c
Jan Tusil <jan.tusil@proton.me>
2025-08-19 15:47:18 +0100

    more flake refactoring and cleanup

--- flake.nix
+++ flake.nix
@@ -35,140 +35,115 @@
          inherit libminuskaSrcFun;
        };
 
-       # The parsers in `languages/*` depend on these.
-       example_languages_parser_deps = [
+
+       groupFun = { pkgs, coqPackages, ocamlPackages  }: (
+       let
+         coq-minuska = coqMinuskaFun { inherit coqPackages; };
+         libminuska-src = libminuskaSrcFun { inherit coqPackages; };
+         minuska = minuskaFun { inherit coqPackages; inherit ocamlPackages; };
+
+         # The parsers in `languages/*` depend on these.
+         example_languages_parser_deps = [
             pkgs.dune_3
-            pkgs.ocamlPackages.menhir
-            pkgs.ocamlPackages.findlib
-            pkgs.ocamlPackages.core
-            pkgs.ocamlPackages.core_unix
-            pkgs.ocamlPackages.ppx_jane
-            pkgs.ocaml
-        ];
+            ocamlPackages.menhir
+            ocamlPackages.findlib
+            ocamlPackages.core
+            ocamlPackages.core_unix
+            ocamlPackages.ppx_jane
+            ocamlPackages.ocaml
+         ];
+
+         bench-standalone = pkgs.stdenv.mkDerivation {
+           name = "bench-standalone";
+           src = ./bench-standalone;
+           nativeBuildInputs = [
+             minuska
+             pkgs.dune_3
+             pkgs.time
+             coqPackages.coqide # for user builtins
+             coqPackages.coq
+            ] ++ example_languages_parser_deps;
+            buildInputs = [
+              coq-minuska
+            ];
+           };
+
+         # Does not work these days
+         bench-hybrid = pkgs.stdenv.mkDerivation {
+           name = "bench-hybrid";
+           src = ./bench-hybrid;
+           nativeBuildInputs = [
+             minuska
+             pkgs.dune_3
+             pkgs.time
+           ] ++ example_languages_parser_deps;
+         };
+
+         minuska-docker = pkgs.dockerTools.buildImage {
+           name = "minuska-docker";
+           config = {
+             Cmd = [ "${minuska}/bin/minuska" ];
+            };
+         };
+
+         minuska-bundle-rpm = bundlers.bundlers.${system}.toRPM minuska;
+         minuska-bundle-deb = bundlers.bundlers.${system}.toDEB minuska;
+       in
+       {
+         inherit coq-minuska;
+         inherit libminuska-src;
+         inherit minuska;
+         inherit bench-standalone;
+         inherit bench-hybrid;
+         inherit minuska-docker;
+         inherit minuska-bundle-rpm;
+         inherit minuska-bundle-deb;
+
+         default = minuska;
+
+         devShells = { 
+           # For developing Minuska
+           coq-minuska = pkgs.mkShell {
+             inputsFrom = [coq-minuska];
+             packages = [
+               coq-minuska.coqPackages.coq-lsp 
+               coq-minuska.coqPackages.coqide 
+             ];
+           };
+           # For using Minuska
+           with-minuska = pkgs.mkShell {
+             packages = [minuska minuska.coqPackages.coq-lsp minuska.coqPackages.coq];
+           };
+           # For developing bench-standalone
+           bench-standalone = pkgs.mkShell {
+             inputsFrom = [bench-standalone];
+             packages = [coq-minuska.coqPackages.coq-lsp coq-minuska.coqPackages.coqide];
+           };
+         };
+       });
+
 
       in {
-        # Contains compiled Coq sources of Minuska
-        packages.coq-minuska = coqMinuskaFun {
-          coqPackages = pkgs.coqPackages_8_20;
-        };
-        
-        # Contains OCaml sources of Minuska,
-        # including those extracted from Coq
-        packages.libminuska-src = libminuskaSrcFun {
-          coqPackages = pkgs.coqPackages_8_20;
-        };
-
-        # Contains compiled Minuska executable
-        packages.minuska = minuskaFun { 
+        legacyPackages.minuskaGroupCoq_8_20 = groupFun {
+          inherit pkgs;
           coqPackages = pkgs.coqPackages_8_20;
           ocamlPackages = pkgs.ocamlPackages;
         };
 
-
-        packages.bench-standalone
-        = pkgs.stdenv.mkDerivation {
-          name = "bench-standalone";
-          src = ./bench-standalone;
-          nativeBuildInputs = [
-            self.outputs.packages.${system}.minuska
-            pkgs.dune_3
-            pkgs.time
-            pkgs.coqPackages_8_20.coqide # for user builtins
-            pkgs.coqPackages_8_20.coq
-          ] ++ example_languages_parser_deps;
-          buildInputs = [
-            self.outputs.packages.${system}.coq-minuska
-          ];
-        };
-
-        packages.bench-hybrid
-        = pkgs.stdenv.mkDerivation {
-          name = "bench-hybrid";
-          src = ./bench-hybrid;
-          nativeBuildInputs = [
-            self.outputs.packages.${system}.minuska
-            pkgs.dune_3
-            pkgs.time
-          ] ++ example_languages_parser_deps;
-        };
-
-        packages.minuska-docker
-        = pkgs.dockerTools.buildImage {
-          name = "minuska-docker";
-          config = {
-            Cmd = [ "${self.outputs.packages.${system}.minuska}/bin/minuska" ];
-          };
-        };
-
-        packages.minuska-bundle-rpm
-        = bundlers.bundlers.${system}.toRPM self.outputs.packages.${system}.minuska;
-
-        packages.minuska-bundle-deb
-        = bundlers.bundlers.${system}.toDEB self.outputs.packages.${system}.minuska;
-
-        packages.default = self.outputs.packages.${system}.minuska;
-        
-        devShells = {
-          
-          # For developing Minuska
-          coq-minuska =
-            let
-              coq-minuska = self.outputs.packages.${system}.coq-minuska;
-            in
-              pkgs.mkShell {
-                inputsFrom = [coq-minuska];
-                packages = [
-                  coq-minuska.coqPackages.coq-lsp 
-                  coq-minuska.coqPackages.coqide 
-                ];
-              };
-
-          # For using Minuska
-          with-minuska =
-            let
-              minuska = self.outputs.packages.${system}.minuska;
-            in
-              pkgs.mkShell {
-                packages = [minuska minuska.coqPackages.coq-lsp minuska.coqPackages.coq];
-              };
-
-
-          # For developing bench-standalone
-          bench-standalone =
-           let
-             bench-standalone = self.outputs.packages.${system}.bench-standalone;
-             coq-minuska = self.outputs.packages.${system}.coq-minuska;
-           in
-             pkgs.mkShell {
-               inputsFrom = [bench-standalone];
-               packages = [coq-minuska.coqPackages.coq-lsp coq-minuska.coqPackages.coqide];
-             };
+        legacyPackages.minuskaGroupDefault = self.outputs.legacyPackages.${system}.minuskaGroupCoq_8_20;
+        packages.coq-minuska        = self.outputs.legacyPackages.${system}.minuskaGroupDefault.coq-minuska;
+        packages.libminuska-src        = self.outputs.legacyPackages.${system}.minuskaGroupDefault.libminuska-src;
+        packages.minuska        = self.outputs.legacyPackages.${system}.minuskaGroupDefault.minuska;
 
 
 
-          languages-in-coq =
-            let
-              languages-in-coq = self.outputs.packages.${system}.languages-in-coq;
-            in
-              pkgs.mkShell {
-                inputsFrom = [languages-in-coq];
-                packages = [
-                  languages-in-coq.coqPackages.coq-lsp
-                ];
-              };
-
-          examples-coq =
-            let
-              examples-coq = self.outputs.packages.${system}.examples-coq;
-            in
-              pkgs.mkShell {
-                inputsFrom = [examples-coq];
-                packages = [
-                  examples-coq.coqPackages.coq-lsp
-                ];
-              };
-
-        };
+        packages.bench-standalone   = self.outputs.legacyPackages.${system}.minuskaGroupDefault.bench-standalone;
+        packages.bench-hybrid       = self.outputs.legacyPackages.${system}.minuskaGroupDefault.bench-hybrid;
+        packages.minuska-docker     = self.outputs.legacyPackages.${system}.minuskaGroupDefault.minuska-docker;
+        packages.minuska-bundle-rpm = self.outputs.legacyPackages.${system}.minuskaGroupDefault.minuska-bundle-rpm;
+        packages.minuska-bundle-deb = self.outputs.legacyPackages.${system}.minuskaGroupDefault.minuska-bundle-deb;
+        packages.default            = self.outputs.legacyPackages.${system}.minuskaGroupDefault.default;
+        devShells                   = self.outputs.legacyPackages.${system}.minuskaGroupDefault.devShells;
       }
     )
   );
