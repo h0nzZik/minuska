ab4e8b015a1d60eaaffb7d608edf4692e58a7dce
Jan Tusil <jan.tusil@proton.me>
2025-08-19 13:39:53 +0100

    extract nix/coqMinuskaFun.nix

--- flake.nix
+++ flake.nix
@@ -11,52 +11,11 @@
   outputs = { self, nixpkgs, flake-utils, bundlers }: (
     flake-utils.lib.eachDefaultSystem (system:
       let
-
-        pkgs = nixpkgs.legacyPackages.${system};
-        
-        coqMinuskaFun = { coqPackages }: (
-           let coqVersion = coqPackages.coq.coq-version; in
-           let stdpp = coqPackages.stdpp; in
-           let coqLibraries = [
-             stdpp
-           ]; in
-           let coqPlugins = [
-              coqPackages.equations
-              coqPackages.QuickChick 
-           ]; in
-           let bothNativeAndOtherInputs = [
-              coqPackages.coq
-           ] ++ coqLibraries ++ coqPlugins ; in
-           let wrapped = coqPackages.callPackage  ( { coq, stdenv }: coqPackages.mkCoqDerivation {
-
-            useDune = true; 
-            pname = "minuska";
-            version = "0.6.0";
-            src = ./coq-minuska;
-            duneVersion = "3";
- 
-            nativeBuildInputs = [
-              pkgs.dune_3
-              pkgs.time # for QuickChick
-            ] ++ bothNativeAndOtherInputs;
-
-            passthru = {
-              inherit coqPackages;
-              inherit coqLibraries;
-              inherit coqPlugins;
-	    };
-
-            buildPhase = ''
-              runHook preBuild
-              dune build @all theories/Minuska.html ''${enableParallelBuilding:+-j $NIX_BUILD_CORES}
-              runHook postBuild
-            '';
-
-            #installFlags = [ "COQLIB=$(out)/lib/coq/${coqPackages.coq.coq-version}/" ];
-
-          } ) { };  in
-          (pkgs.enableDebugging wrapped)
-       );
+        pkgs = nixpkgs.legacyPackages.${system}; 
+        coqMinuskaFun = import ./nix/coqMinuskaFun.nix { 
+          inherit pkgs;
+          src = ./coq-minuska;
+        }; 
 
       # OCaml sources
       libminuskaSrcFun = { coqPackages }: (
---
+++ nix/coqMinuskaFun.nix
@@ -0,0 +1,44 @@
+{ pkgs, src }:
+{ coqPackages }:
+
+let coqVersion = coqPackages.coq.coq-version; in
+let stdpp = coqPackages.stdpp; in
+let coqLibraries = [
+  stdpp
+]; in
+let coqPlugins = [
+   coqPackages.equations
+   coqPackages.QuickChick 
+]; in
+let bothNativeAndOtherInputs = [
+   coqPackages.coq
+] ++ coqLibraries ++ coqPlugins ; in
+let wrapped = coqPackages.callPackage  ( { coq, stdenv }: coqPackages.mkCoqDerivation {
+  useDune = true; 
+  pname = "minuska";
+  version = "0.6.0";
+  duneVersion = "3";
+
+  inherit src;
+ 
+  nativeBuildInputs = [
+    pkgs.dune_3
+    pkgs.time # for QuickChick
+  ] ++ bothNativeAndOtherInputs;
+
+  passthru = {
+    inherit coqPackages;
+    inherit coqLibraries;
+    inherit coqPlugins;
+  };
+
+  buildPhase = ''
+    runHook preBuild
+    dune build @all theories/Minuska.html ''${enableParallelBuilding:+-j $NIX_BUILD_CORES}
+    runHook postBuild
+  '';
+
+    #installFlags = [ "COQLIB=$(out)/lib/coq/${coqPackages.coq.coq-version}/" ];
+
+} ) { }; in
+(pkgs.enableDebugging wrapped)
